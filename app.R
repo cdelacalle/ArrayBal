8155e32688f9c18c49230139ceaac7b02ab01d19
Carlos de la Calle Fabregat <55692347+cdelacalle@users.noreply.github.com>
2021-05-16 18:16:51 +0200

    commit 3
    Results, Part II -- PEC

--- app.R
+++ app.R
@@ -1,6 +1,1 @@
-# citar los paper de bioconductor con setrepositories() antes de colgar la aplicación a la nube.
-
-source("/disco1/Project_Shiny/Project_Shiny/app_functions.R")
-
 shinyApp(ui = ui, server = server)
-        
